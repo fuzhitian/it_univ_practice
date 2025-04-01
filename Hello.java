a2f791d99515e339bba9de437f72b58fd5d49139
fuzhitian <rieriyofu@gmail.com>
2025-04-02 07:47:07 +0900

    練習した

--- Hello.java
+++ Hello.java
@@ -1,5 +1,22 @@
 public class Hello{
     public static void main(String[] args){
+        filter();
         System.out.println("hello world");
+    }
+
+    // stream例
+    public static void stream(){
+        List<String> list = Arrays.asList("a", "b", "c");
+        list.stream().forEach(System.out::println);
+    }
+    // 特定の要素を抽出
+    public static void filter(){
+        List<String> list = Arrays.asList("a", "b", "c");
+        list.stream().filter(s -> s.equals("a")).forEach(System.out::println);
+    }
+    // 要素変換
+    public static void map(){
+        List<String> list = Arrays.asList("a", "b", "c");
+        list.stream().map(s -> s.toUpperCase()).forEach(System.out::println);
     }
 }
